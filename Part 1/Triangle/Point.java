f5a20bad6785dea12153f7ae5e4bea1e03bc65df
Anastasia Mayorova <90724127+EternityRei@users.noreply.github.com>
2022-02-21 20:55:53 +0200

    Create Point.java

---
+++ Part 1/Triangle/Point.java
@@ -0,0 +1,42 @@
+package com.epam.rd.autotasks.triangle;
+
+class Point {
+    private double x;
+    private double y;
+
+
+    public Point(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point)) return false;
+
+        Point point = (Point) o;
+
+        if (Double.compare(point.x, x) != 0) return false;
+        if (Double.compare(point.y, y) != 0) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = x != +0.0d ? Double.doubleToLongBits(x) : 0L;
+        result = (int) (temp ^ (temp >>> 32));
+        temp = y != +0.0d ? Double.doubleToLongBits(y) : 0L;
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
