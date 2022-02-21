28d8ba3ad8e704826c3fc351eed26bb6379f1524
Anastasia Mayorova <90724127+EternityRei@users.noreply.github.com>
2022-02-21 20:50:29 +0200

    Create CarouselRun.java

---
+++ Part 1/HalvingCarousel/CarouselRun.java
@@ -0,0 +1,43 @@
+package com.epam.rd.autotasks;
+
+public class CarouselRun {
+    private final int[] storage;
+    private int index;
+    private boolean isFinished = false;
+    private final DecrementingCarousel carousel;
+
+    public CarouselRun(int[] storage, DecrementingCarousel carousel){
+        this.storage = storage;
+        this.carousel = carousel;
+        if(storage.length == 0 || storage[index] ==0)
+            isFinished = true;
+    }
+
+    public void setNextIndex(){
+        int count = 0;
+        do{
+            count++;
+            index = (index + 1) % storage.length;
+            if(storage[index] != 0)
+                return;
+        } while(count <= storage.length);
+        isFinished = true;
+        index = -1;
+    }
+
+    public int next(){
+        if(isFinished){
+            return -1;
+        } else {
+            int element = storage[index];
+            storage[index] = carousel.modifyElement(storage[index]);
+            setNextIndex();
+            return element;
+        }
+    }
+
+    public boolean isFinished(){
+        return isFinished;
+    }
+
+}
